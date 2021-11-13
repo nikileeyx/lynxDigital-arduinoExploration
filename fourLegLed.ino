118a864df2c017aa9bca1e21f34a4ca56a4cc3f0
codingLynx <52266453+nikileeyx@users.noreply.github.com>
2021-11-13 17:24:51 +0800

    Update fourLegLed.ino

--- fourLegLed.ino
+++ fourLegLed.ino
@@ -32,4 +32,3 @@ void RGB_color(int red_light_value, int green_light_value, int blue_light_value)
   analogWrite(green_light_pin, green_light_value);
   analogWrite(blue_light_pin, blue_light_value);
 }
-
